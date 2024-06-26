28899708a24c516a5bb4658e695b942b6f60a4f1
“CS21B029” <cs21b029@iittp.ac.in>
2024-06-26 18:22:47 +0530

    modified address ui and added validations to mailing details

--- frontend/src/Routes/Address.tsx
+++ frontend/src/Routes/Address.tsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Button, FormControl, FormGroup, TextField, Grid, Typography, Checkbox } from '@mui/material';
+import { Box, Button, FormControl, FormGroup, TextField, Grid, Typography, Checkbox, FormHelperText, InputLabel, MenuItem, Select } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import useStore from '../Store/Store';
-import Select, { SingleValue } from 'react-select';
 import '../Styles/Address.css';
 
 interface AddressProps {
@@ -17,7 +16,7 @@ interface CountryOption {
 }
 
 const Address: React.FC<AddressProps> = ({ handleNext, handleBack }) => {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState, control } = useForm();
   const setAddress = useStore((state) => state.setAddress);
   const address = useStore((state) => state.address);
   const [streetNumberError, setStreetNumberError] = useState('');
@@ -25,7 +24,9 @@ const Address: React.FC<AddressProps> = ({ handleNext, handleBack }) => {
   const [zipError, setZipError] = useState('');
   const [mZipError, setMZipError] = useState('');
   const [countries, setCountries] = useState<CountryOption[]>([]);
-  const [selectedCountry, setSelectedCountry] = useState<SingleValue<CountryOption>>(null);
+  const [selectedCountry, setSelectedCountry] = useState<string>('');
+  const [sameAddress, setSameAddress] = useState<boolean | null>(null); // State to track if mailing address is the same
+  const [display, setDisplay] = useState<boolean>(false);
   const navigate = useNavigate();
   const { errors } = formState;
 
@@ -36,7 +37,6 @@ const Address: React.FC<AddressProps> = ({ handleNext, handleBack }) => {
       .then((response) => response.json())
       .then((data) => {
         setCountries(data.countries);
-        // setSelectedCountry(data.userSelectValue);
       });
   }, []);
 
@@ -88,20 +88,25 @@ const Address: React.FC<AddressProps> = ({ handleNext, handleBack }) => {
 
   const onSubmit = (data: any) => {
     console.log(selectedCountry);
-    data.mCountry = selectedCountry?.value || '';
-    if (data.mStreetNumber === '' && data.mStreetName === '' && data.mApartUnit === '' && data.mTown === '' && data.mState === '' && data.mZip === '' && data.mCountry === '') {
-      data.mStreetNumber = data.streetNumber;
-      data.mStreetName = data.streetName;
-      data.mApartUnit = data.apartUnit;
-      data.mTown = data.city;
-      data.mState = data.state;
-      data.mZip = data.zip;
-      data.mCountry = 'US';
-    }
+    data.mCountry = selectedCountry || '';
+    if (sameAddress === null || sameAddress === undefined || (sameAddress === false && data.mCountry === '')) {
+      setDisplay(true);
+      return;
+    } else {
+      if (sameAddress === true) {
+        data.mStreetNumber = data.streetNumber;
+        data.mStreetName = data.streetName;
+        data.mApartUnit = data.apartUnit;
+        data.mTown = data.city;
+        data.mState = data.state;
+        data.mZip = data.zip;
+        data.mCountry = 'US';
+      }
 
-    console.log(data);
-    setAddress(data);
-    handleNextAddress();
+      console.log(data);
+      setAddress(data);
+      handleNextAddress();
+    }
   };
 
   return (
@@ -208,7 +213,7 @@ const Address: React.FC<AddressProps> = ({ handleNext, handleBack }) => {
                   },
                 })}
                 error={!!errors.zip || !!zipError}
-                helperText={zipError || (errors.zip && 'Zip code should be in ##### or #####-#### format')}
+                helperText={zipError || (errors.zip?.message === 'Invalid Zip Code' ? 'Zip code should be in ##### or #####-#### format' : '')}
                 inputProps={{
                   inputMode: 'numeric',
                   onKeyPress: handleZipKeyPress,
@@ -220,93 +225,171 @@ const Address: React.FC<AddressProps> = ({ handleNext, handleBack }) => {
             <Checkbox {...register('military')} />
             <Typography variant="body1" className='addr-note-text'>I am a member of the military</Typography>
           </Box>
-          <Box className='addr-mail-heading'>
-            <Typography variant="h5" >Mailing Address</Typography>
-            <Typography variant="body1" className='addr-note-text-heading'>(Please provide your mailing address if different from residential address)</Typography>
+          <Box mt={1} className='addr-same-note'>
+            <Typography variant="body1" className='addr-note-text'>Is your Mailing Address same as Residential Address?</Typography>
+            <Checkbox
+              checked={sameAddress === true}
+              onChange={() => setSameAddress(true)}
+            />
+            <Typography variant="body1" className='addr-note-text'>Yes</Typography>
+            <Checkbox
+              checked={sameAddress === false}
+              onChange={() => setSameAddress(false)}
+            />
+            <Typography variant="body1" className='addr-note-text'>No</Typography>
+            {(display === true && (sameAddress === null || sameAddress === undefined)) && (
+              <FormHelperText className='addr-error' error>You Must Select an Option</FormHelperText>
+            )}
           </Box>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                className="add-detail"
-                variant="outlined"
-                label='Street Number'
-                defaultValue={address?.mStreetNumber}
-                {...register('mStreetNumber')}
-                error={!!mStreetNumberError}
-                helperText={mStreetNumberError}
-                inputProps={{
-                  inputMode: 'numeric',
-                  pattern: '[0-9]*',
-                  onKeyPress: handleMStreetNumberKeyPress,
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                className="add-detail"
-                variant="outlined"
-                label='Street Name'
-                defaultValue={address?.mStreetName}
-                {...register('mStreetName')}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                className="add-detail"
-                variant="outlined"
-                label='Apartment/Unit'
-                defaultValue={address?.mApartUnit}
-                {...register('mApartUnit')}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                className="add-detail"
-                variant="outlined"
-                label='City'
-                defaultValue={address?.mTown}
-                {...register('mTown')}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                className="add-detail"
-                variant="outlined"
-                label='State'
-                defaultValue={address?.mState}
-                {...register('mState')}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                className="add-detail"
-                variant="outlined"
-                label='Zip Code'
-                defaultValue={address?.mZip}
-                {...register('mZip')}
-                error={!!mZipError}
-                helperText={mZipError}
-                inputProps={{
-                  inputMode: 'numeric',
-                  onKeyPress: handleMZipKeyPress,
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Select
-                placeholder="Country"
-                options={countries}
-                defaultValue={selectedCountry}
-                onChange={setSelectedCountry}
-                className="add-detail-country"
-              />
-            </Grid>
-          </Grid>
-          <Box mt={2}>
-            <Button variant="contained" onClick={handleBackAddress} className="back-button">Back</Button>
-            <Button variant="contained" color="primary" type="submit">Next</Button>
-          </Box>
+          {sameAddress === false && (
+            <React.Fragment>
+              <Box className='addr-mail-heading'>
+                <Typography variant="h5" className='addr-mail-heading-text'>Mailing Address</Typography>
+              </Box>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    className="add-detail"
+                    variant="outlined"
+                    label={typeof errors.mStreetNumber?.message === 'string' ? errors.mStreetNumber.message : 'Street Number*'}
+                    defaultValue={address?.mStreetNumber}
+                    {...register('mStreetNumber', {
+                      required: {
+                        value: true,
+                        message: 'Street Number is required',
+                      },
+                    })}
+                    error={!!errors.mStreetNumber || !!mStreetNumberError}
+                    helperText={mStreetNumberError}
+                    inputProps={{
+                      inputMode: 'numeric',
+                      pattern: '[0-9]*',
+                      onKeyPress: handleMStreetNumberKeyPress,
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    className="add-detail"
+                    variant="outlined"
+                    label={typeof errors.mStreetName?.message === 'string' ? errors.mStreetName.message : 'Street Name*'}
+                    defaultValue={address?.mStreetName}
+                    {...register('mStreetName', {
+                      required: {
+                        value: true,
+                        message: 'Street Name is required',
+                      },
+                    })}
+                    error={!!errors.mStreetName}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    className="add-detail"
+                    variant="outlined"
+                    label={typeof errors.mApartUnit?.message === 'string' ? errors.mApartUnit.message : 'Apartment/Unit*'}
+                    defaultValue={address?.mApartUnit}
+                    {...register('mApartUnit', {
+                      required: {
+                        value: true,
+                        message: 'Apartment/Unit is required',
+                      },
+                    })}
+                    error={!!errors.mApartUnit}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    className="add-detail"
+                    variant="outlined"
+                    label={typeof errors.mTown?.message === 'string' ? errors.mTown.message : 'City*'}
+                    defaultValue={address?.mTown}
+                    {...register('mTown', {
+                      required: {
+                        value: true,
+                        message: 'City is required',
+                      },
+                    })}
+                    error={!!errors.mTown}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    className="add-detail"
+                    variant="outlined"
+                    label={typeof errors.mState?.message === 'string' ? errors.mState.message : 'State*'}
+                    defaultValue={address?.mState}
+                    {...register('mState', {
+                      required: {
+                        value: true,
+                        message: 'State is required',
+                      },
+                    })}
+                    error={!!errors.mState}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    className="add-detail"
+                    variant="outlined"
+                    label={typeof errors.mZip?.message === 'string' ? errors.mZip.message : 'Zip Code*'}
+                    defaultValue={address?.mZip}
+                    {...register('mZip', {
+                      required: {
+                        value: true,
+                        message: 'Zip Code is required',
+                      },
+                      pattern: {
+                        value: /^[0-9]{5}(-[0-9]{4})?$/,
+                        message: 'Invalid Zip Code',
+                      },
+                    })}
+                    error={!!errors.mZip || !!mZipError}
+                    helperText={mZipError || (errors.mZip?.message === 'Invalid Zip Code' ? 'Zip code should be in ##### or #####-#### format' : '')}
+                    inputProps={{
+                      inputMode: 'numeric',
+                      onKeyPress: handleMZipKeyPress,
+                    }}
+                  />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                  <FormControl variant='outlined' fullWidth error={!!errors.mCountry} className='add-detail'>
+                    <InputLabel id="mCountry-label">{errors.mCountry ? 'Country is Required' : 'Country*'}</InputLabel>
+                    <Controller
+                      name='mCountry'
+                      control={control}
+                      rules={{ required: 'Country is required' }}
+                      defaultValue={address?.mCountry}
+                      render={({ field }) => (
+                        <Select
+                          {...field}
+                          label={typeof errors.mCountry?.message === 'string' ? errors.mCountry.message : 'Country*'}
+                          value={selectedCountry}
+                          onChange={(e) => {
+                            setSelectedCountry(e.target.value);
+                            field.onChange(e.target.value);
+                          }}
+                        >
+                          {countries.map((country) => (
+                            <MenuItem key={country.value} value={country.value}>
+                              {country.label}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      )}
+                    />
+                    {errors.mCountry && <FormHelperText error>Country is Required</FormHelperText>}
+                  </FormControl>
+                </Grid>
+              </Grid>
+            </React.Fragment>
+          )}
         </FormGroup>
+        <Box mt={2}>
+          <Button variant="contained" onClick={handleBackAddress} className="back-button">Back</Button>
+          <Button variant="contained" color="primary" type="submit" >Next</Button>
+        </Box>
       </FormControl>
     </form>
   );
